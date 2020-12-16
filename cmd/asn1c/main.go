a0e920ceaddd0b6f8689635c719a397b6bd666bc
Sandeep Bagchi <sandeep.bagchi@gmail.com>
2020-12-16 23:18:11 +0530

    Initial commit

--- cmd/asn1c/main.go
+++ cmd/asn1c/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 func main() {
-	$END$
+	var (
+		filename = flag.String("file", "", "Abstract Syntax Notation 1 file")
+	)
+	flag.Parse()
+	if len(*filename) == 0 {
+		fmt.Println("Error: ", "input asn1 file required ...")
+		os.Exit(0)
+	}
 }
--- parser.go
+++ parser.go
@@ -1,1 +1,5 @@
 package asn1c_go
+
+func Parse(filename string) {
+
+}
