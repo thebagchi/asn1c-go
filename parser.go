e6cc67e8f50b3d69d6bbe56cab881793383a5112
Sandeep Bagchi <sandeep.bagchi@gmail.com>
2021-02-18 18:47:09 +0530

    Specification

--- parser.go
+++ parser.go
@@ -1,19 +1,40 @@
 package asn1c_go
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
-	"os"
+	"io/ioutil"
+	"regexp"
 )
 
+type ModuleDefinition struct {
+}
+
+func RemoveBlanks(buffer []byte) []byte {
+	regex := regexp.MustCompile("(?m)^\\s*$[\r\n]*")
+	return bytes.Trim(regex.ReplaceAll(buffer, []byte("")), "\r\n")
+}
+
+func RemoveBlockComment(content []byte) []byte {
+	comment := regexp.MustCompile(`/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/`)
+	return comment.ReplaceAll(content, []byte(""))
+}
+
+func RemoveLineComment(content []byte) []byte {
+	comment := regexp.MustCompile(`--.*`)
+	return comment.ReplaceAll(content, []byte(""))
+}
+
+func RemoveComments(content []byte) []byte {
+	return RemoveBlanks(RemoveLineComment(RemoveBlockComment(content)))
+}
+
 func Parse(filename string) error {
-	file, err := os.Open(filename)
+	data, err := ioutil.ReadFile(filename)
 	if nil != err {
 		return err
 	}
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
+	data = RemoveComments(data)
+	fmt.Println(string(data))
 	return nil
 }
